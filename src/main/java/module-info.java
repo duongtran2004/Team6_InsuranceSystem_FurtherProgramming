314b607ec2fcb0b40fd4cb697a1fc2d53e783e1a
trungluong0806 <141601693+trungluong080699@users.noreply.github.com>
2024-04-23 19:13:00 +0700

    Add persistence.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.insurancemanagementapplication {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
 
 
     opens org.example.insurancemanagementapplication to javafx.fxml;
