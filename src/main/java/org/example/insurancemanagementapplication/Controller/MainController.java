9208c5cfb3beffbff320445e30609bd2ea5325ae
trungluong0806 <>
2024-04-27 05:55:39 +0700

    Adding Login Method

--- src/main/java/org/example/insurancemanagementapplication/Controller/MainController.java
+++ src/main/java/org/example/insurancemanagementapplication/Controller/MainController.java
@@ -1,10 +1,16 @@
 package org.example.insurancemanagementapplication.Controller;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * @author Luong Thanh Trung
@@ -12,7 +18,7 @@ import javafx.scene.control.TextField;
  * @created 27/04/2024 04:50
  * @project InsuranceManagementTeamProject
  */
-public class MainController {
+public class MainController implements Initializable {
     EntityManager entityManager;
 
     public MainController(EntityManager entityManager) {
@@ -26,13 +32,38 @@ public class MainController {
     public void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
-
-    private TextField useridField;
+    @FXML
+    private TextField userIdField;
+    @FXML
     private TextField emailField;
+    @FXML
     private TextField passwordField;
+    @FXML
     private ChoiceBox<String> roleSelectionBox;
-    private Button loginButton;
+    @FXML
+    private Button logInButton;
+    @FXML
     private Label errorContainer;
+    private String[] roles = {"System Admin", "Insurance Manager", "Insurance Surveyor", "Policy Owner", "Policy Holder", "Dependant"};
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        roleSelectionBox.getItems().addAll(roles);
+    }
+
+    public void logIn(){
+        String email = emailField.getText();
+        String userId = userIdField.getText();
+        String password = passwordField.getText();
+        String role = roleSelectionBox.getValue();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try{
+            transaction.begin();
+        }finally {
+            if (transaction.isActive()){
+                transaction.rollback();
+            }
+        }
+    }
 }
