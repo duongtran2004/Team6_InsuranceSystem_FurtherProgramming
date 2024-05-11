7693f9a1578526f2248af6abbf25fe8fe92ac14e
duongtran2004 <S3979381@rmit.edu.vn>
2024-05-11 09:57:37 +0700

    Finish basic yearly rate calculation, missing projected revenue and rank policy owner by total yearly rate (vip customer)

--- src/main/java/org/example/insurancemanagementapplication/Interfaces/YearlyRateCalculation.java
+++ src/main/java/org/example/insurancemanagementapplication/Interfaces/YearlyRateCalculation.java
@@ -1,55 +1,110 @@
 package org.example.insurancemanagementapplication.Interfaces;
 
+import Entity.PolicyOwner;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+
+import java.util.List;
+
 public interface YearlyRateCalculation {
 
     //FOR POLICY OWNER
-    //count number of policy holders of a policy owner
-//
-//    public static int countPolicyHoldersOfAPolicyOwner(EntityManager entityManager, String policyOwnerID) {
-//        Query query = entityManager.createQuery("SELECT COUNT(c) FROM Beneficiaries c WHERE c.policyOwnerId = ?1 AND c.type = 'PH'");
-//        query.setParameter(1, policyOwnerID);
-//        long policyHolderCount = (long) query.getSingleResult();
-//        return (int) policyHolderCount;
-//    }
-//
-//
-//    //count number of dependants of a policy owner
-//
-//    public static int countDependantsOfAPolicyOwner(EntityManager entityManager, String policyOwnerID) {
-//        Query query = entityManager.createQuery("SELECT COUNT(c) FROM Beneficiaries c WHERE c.policyOwnerId = ?1 AND c.type = 'DE'");
-//        query.setParameter(1, policyOwnerID);
-//        long dependantCount = (long) query.getSingleResult();
-//        return (int) dependantCount;
-//    }
-//
-//    public static int calculateYearlyRateOfAPolicyOwner(EntityManager entityManager, String policyOwnerID) {
-//        int dependantCount = countDependantsOfAPolicyOwner(entityManager, policyOwnerID);
-//        int policyHolderCount = countPolicyHoldersOfAPolicyOwner(entityManager, policyOwnerID);
-//        PolicyOwner policyOwner = entityManager.getReference(PolicyOwner.class, policyOwnerID); //lazy loading object
-//        int yearlyRate = policyOwner.getYearlyRate();
-//        int totalYearlyRate = yearlyRate * policyHolderCount + yearlyRate * (60 / 100) * dependantCount;
-//        return totalYearlyRate;
-//    }
-//
-//
-//    //FOR SYSTEM ADMIN
-//    //calculate total yearly rate from all policy owner
-//    public static int calculateTotalYearlyRateOfAllPolicyOwners(EntityManager entityManager) {
-//
-//        Query query = entityManager.createQuery("SELECT SUM(c.yearlyRate) FROM PolicyOwner c ");
-//        int totalYearlyRate = (int) query.getSingleResult();
-//        return totalYearlyRate;
-//    }
-//
-//
-//    //rank all policy owners by total yearly rate (vip customer)
-//
-//    //more advance feature if we have time:
-//
-//    //calculate prospect revenue
-//    // by a time range
-//    // by this week, by this month, by this quarter, by this year
-//
-//
+
+
+
+
+
+
+
+    //    count number of policy holders of a policy owner
+
+    public static int countPolicyHoldersOfAPolicyOwner(EntityManager entityManager, String policyOwnerID) {
+        Query query = entityManager.createQuery("SELECT COUNT(c) FROM Beneficiaries c WHERE c.policyOwnerId = ?1 AND c.type = 'PH'");
+        query.setParameter(1, policyOwnerID);
+        long policyHolderCount = (long) query.getSingleResult();
+        return (int) policyHolderCount;
+    }
+
+
+    //count number of dependants of a policy owner
+
+    public static int countDependantsOfAPolicyOwner(EntityManager entityManager, String policyOwnerID) {
+        Query query = entityManager.createQuery("SELECT COUNT(c) FROM Beneficiaries c WHERE c.policyOwnerId = ?1 AND c.type = 'DE'");
+        query.setParameter(1, policyOwnerID);
+        long dependantCount = (long) query.getSingleResult();
+        return (int) dependantCount;
+    }
+
+    public static String dividePolicyOwnerIntoPHYearlyRateLevel (int beneficiariesCount){
+        String level = "";
+
+        if (beneficiariesCount < 10){
+            level = "D";
+        }
+        else if (beneficiariesCount < 20){
+            level = "C";
+
+        }
+        else if (beneficiariesCount < 40){
+            level = "B";
+        }
+        else {
+            level = "A";
+        }
+        return level;
+    }
+
+    public static int getPHYearlyRateBasedOnLevel (String level){ //yearlyRate of A Policy Holder
+        int yearlyRate = 0;
+        if (level.equals("D")){
+            yearlyRate = 400000;
+        }
+        else if (level.equals("C")){
+            yearlyRate = 300000;
+        }
+        else if (level.equals("B")){
+            yearlyRate = 150000;
+        }
+        else if (level.equals("A")){
+            yearlyRate = 100000;
+        }
+        return yearlyRate;
+    }
+
+        public static int calculateYearlyRateOfAPolicyOwner(EntityManager entityManager, String policyOwnerID) {
+        int dependantCount = countDependantsOfAPolicyOwner(entityManager, policyOwnerID);
+        int policyHolderCount = countPolicyHoldersOfAPolicyOwner(entityManager, policyOwnerID);
+        int beneficiaryCount = dependantCount + policyHolderCount;
+        String level = dividePolicyOwnerIntoPHYearlyRateLevel(beneficiaryCount);
+        int yearlyRate = getPHYearlyRateBasedOnLevel(level);
+        int totalYearlyRate = yearlyRate * policyHolderCount + yearlyRate * (60 / 100) * dependantCount;
+        return totalYearlyRate;
+    }
+
+
+    //FOR SYSTEM ADMIN
+    //calculate total yearly rate from all policy owner
+    public static int calculateTotalYearlyRateOfAllPolicyOwners(EntityManager entityManager) {
+        int POYearlyRateSum = 0;
+        //get all policy owner and store into a List
+        List<PolicyOwner> policyOwners = CustomerRead.getAllPolicyOwner(entityManager);
+        //loop through each policy owner => perform calculateYearlyRateOfAPolicyOwner
+        for (PolicyOwner policyOwner : policyOwners) {
+            //add up the sum
+            POYearlyRateSum += calculateYearlyRateOfAPolicyOwner(entityManager, policyOwner.getId());
+        }
+        return POYearlyRateSum;
+    }
+
+
+    //rank all policy owners by total yearly rate (vip customer)
+
+    //more advance feature if we have time:
+
+    //calculate prospect revenue
+    // by a time range
+    // by this week, by this month, by this quarter, by this year
+
+
 
 }
